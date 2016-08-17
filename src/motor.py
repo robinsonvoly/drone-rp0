b7775175768037ed9a174f6f5186bd1f715d9fd3
Robinson Vo-Ly <ly_robinson@yahoo.com>
2016-08-16 19:17:40 -0700

    add motor.py

---
+++ src/motor.py
@@ -0,0 +1,40 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+
+dc = 5
+
+servo = GPIO.PWM(11,50)
+servo.start(10)
+print('***Connect Battery & Press ENTER to start')
+res = raw_input()
+servo.ChangeDutyCycle(dc) #servo.ChangeDutyCycle(5)
+print('***Press ENTER to start')
+res = raw_input()
+
+print ('increase > a | decrease > z | save Wh > n | set Wh > h|quit > 9')
+
+cycling = True
+try:
+    while cycling:
+        servo.ChangeDutyCycle(dc)
+        res = raw_input()
+        if res == 'a':
+            dc = dc + 0.05
+        if res == 'z':
+            dc = dc - 0.05
+        if res == 'h':
+            mymotor.setWh()
+        if res == '9':
+            cycling = False
+finally:
+    # shut down cleanly
+    servo.stop()
+    print ("dc var setting is: ")
+    print (dc)
+
+
+print('***Press ENTER to quit')
+res = raw_input()
+servo.stop()
+GPIO.cleanup()
